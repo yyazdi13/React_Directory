b25fb40c0546c618aa1e10495c682fd7f473995f
yasmin yazdi <yyazdi13@gmail.com>
2020-03-07 11:28:07 -0600

    Updates

---
+++ precache-manifest.bab867ea5bf66d6e553744ed3e277583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0e14e512854bf994339a3c7728fcfa",
+    "url": "/React_Directory/index.html"
+  },
+  {
+    "revision": "c405eebaa2210b72c092",
+    "url": "/React_Directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d6218ef158f06f58d74",
+    "url": "/React_Directory/static/js/2.daf56d69.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React_Directory/static/js/2.daf56d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c405eebaa2210b72c092",
+    "url": "/React_Directory/static/js/main.d324590c.chunk.js"
+  },
+  {
+    "revision": "bee2b1558c57a058a222",
+    "url": "/React_Directory/static/js/runtime-main.d084ba91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_Directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
